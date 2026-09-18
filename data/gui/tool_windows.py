67c81ab3f2f6137101af9afd1d8baa95e82e51f7
MAX-EMELKIN <emelm707@gmail.com>
2026-09-18 20:00:39 +0000

    Fix tool_windows re-exports for BatchWindow, GlossaryWindow, SettingsWindow, ChatWindow

--- data/gui/tool_windows.py
+++ data/gui/tool_windows.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 # data/gui/tool_windows.py
+from data.gui.glossary_window import GlossaryWindow, GlossaryEnrichmentDialog, BatchGlossaryImportDialog
+from data.gui.batch_window import BatchWindow
+from data.gui.settings_window import SettingsWindow
+from data.gui.chat_window import ChatWindow
 
 __all__ = [
     "GlossaryWindow",
